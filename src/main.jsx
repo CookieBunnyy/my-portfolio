b0d6a2f7993c466d39658b9b7cd8974a988a88a9
CookieBunnyy <gabriellesiroy25@gmail.com>
2025-10-17 03:54:39 +0800

    Update for GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
-/* src/main.jsx */
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css'
+// main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // if using routing
+import App from "./App";
+import "./index.css"; // your global styles
 
+// Replace 'your-repo-name' with your GitHub repo name
+const basename = "/my-portfolio/";
 
-createRoot(document.getElementById('root')).render(
-<React.StrictMode>
-<App />
-</React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter basename={basename}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
